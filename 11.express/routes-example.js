9c8551345f60b9aaaf29b6ce458711733b5e1156
Dhanushkumar G <dhanushkumaramk@gmail.com>
2025-05-20 07:59:05 +0530

    routes completed

--- 11.express/routes-example.js
+++ 11.express/routes-example.js
@@ -6,9 +6,8 @@ app.get("/", (req, res) => {
     res.send("Welcome to my root page");
 })
 
-// get all products
-app.get('/products', (req, res) => {
-    const products = [
+// products
+  const products = [
         {
             id : 1,
             name : "Product 1",
@@ -23,8 +22,20 @@ app.get('/products', (req, res) => {
         },
     ]
 
+// get all products
+app.get('/products', (req, res) => {
     res.json(products)
+})
 
+// get specific products
+// (dynamic routing)
+app.get('/products/:productId', (req, res) => {
+    const ProductID = parseInt(req.params.productId) // get the id on paramater
+    const getSingleProduct = products.find(product => product.id === ProductID)
+    if(getSingleProduct)
+        res.json(getSingleProduct);
+    else
+        res.status(400).json({message : "Id not found"})
 })
 
 const port = 3000
